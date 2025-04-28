c56d4f9c8ed55c5060217b35ebbaa9d4d2ec53c3
siddhartha-p <siddhartha.pradhan.08@gmail.com>
2025-04-28 15:11:10 +0545

    documentation code added

---
+++ src/main/java/com/clean/architecture/core/security/AuthenticationProviderUserPassword.java
@@ -0,0 +1,23 @@
+package com.clean.architecture.core.security;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.security.authentication.AuthenticationFailureReason;
+import io.micronaut.security.authentication.AuthenticationRequest;
+import io.micronaut.security.authentication.AuthenticationResponse;
+import io.micronaut.security.authentication.provider.HttpRequestAuthenticationProvider;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class AuthenticationProviderUserPassword <B> implements HttpRequestAuthenticationProvider<B> {
+    @Override
+    public @NonNull AuthenticationResponse authenticate(@Nullable HttpRequest<B> httpRequest,
+                                                        @NonNull AuthenticationRequest<String, String> authenticationRequest)
+    {
+        return authenticationRequest.getIdentity().equals("siddhartha08") && authenticationRequest.getSecret().equals("Hashing Password")
+                ? AuthenticationResponse.success(authenticationRequest.getIdentity())
+                :AuthenticationResponse.failure(AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH);
+
+    }
+
+}
